6f8487f5252bd755e97680b92388dadfc1c154fd
gwh <1595864175@qq.com>
2020-07-01 15:02:29 +0800

    新增：过滤器

--- SpringBoot、Mybatis/SpringBoot/src/main/java/com/muye/kl/App.java
+++ SpringBoot、Mybatis/SpringBoot/src/main/java/com/muye/kl/App.java
@@ -3,9 +3,10 @@ package com.muye.kl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+
 @SpringBootApplication
-@ServletComponentScan
-public class App{
+@ServletComponentScan("com.muye.kl.filter")
+public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
     }
--- SpringBoot、Mybatis/SpringBoot/src/main/java/com/muye/kl/interceptor/SessionInterceptor.java
+++ SpringBoot、Mybatis/SpringBoot/src/main/java/com/muye/kl/interceptor/SessionInterceptor.java
@@ -1,19 +1,22 @@
-package com.muye.kl;
+package com.muye.kl.interceptor;
 
-public class SessionInterceptor implements HandlerInterceptor
-{
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class SessionInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object o) throws Exception {
         System.out.println(request.getRequestURI());
         //登录不做拦截
-        if(request.getRequestURI().equals("/user/login") || request.getRequestURI().equals("/user/login_view"))
-        {
+        if (request.getRequestURI().equals("/user/login") || request.getRequestURI().equals("/user/login_view")) {
             return true;
         }
         //验证session是否存在
         Object obj = request.getSession().getAttribute("_session_user");
-        if(obj == null)
-        {
+        if (obj == null) {
             response.sendRedirect("/user/login_view");
             return false;
         }
