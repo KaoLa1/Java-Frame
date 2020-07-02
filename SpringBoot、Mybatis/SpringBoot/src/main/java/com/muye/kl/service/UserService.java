ec09ed925a1b69b53b1452867db468d4f07ce0d6
gwh <1595864175@qq.com>
2020-07-02 16:43:03 +0800

    新增：SpringCache

--- SpringBoot、Mybatis/SpringBoot/src/main/java/com/muye/kl/service/UserService.java
+++ SpringBoot、Mybatis/SpringBoot/src/main/java/com/muye/kl/service/UserService.java
@@ -3,15 +3,35 @@ package com.muye.kl.service;
 import com.muye.kl.entity.User;
 import org.springframework.stereotype.Service;
 
-@Service
-public class UserService {
+public interface UserService {
 
     /**
      * 获取用户信息
      * @return
      */
-    public User getUser() {
-        // 实际中会根据具体的业务场景，从数据库中查询对应的信息
-        return new User("1", "倪升武", "123456");
-    }
+    public User getUser() ;
+
+    /**
+     * 删除
+     *
+     * @param user 用户对象
+     * @return 操作结果
+     */
+    User saveOrUpdate(User user);
+
+    /**
+     * 添加
+     *
+     * @param id key值
+     * @return 返回结果
+     */
+    User get(Long id);
+
+    /**
+     * 删除
+     *
+     * @param id key值
+     */
+    void delete(Long id);
+
 }
